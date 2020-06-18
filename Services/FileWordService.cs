c537c2c93193e4811b3dadea510f03a728432574
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2020-06-18 16:51:25 +0200

    Refaktoring

--- Services/FileWordService.cs
+++ Services/FileWordService.cs
@@ -3,15 +3,6 @@ using System.IO;
 
 namespace redis_autocomplete
 {
-    public class FakeWordService : IWordService
-    {
-        public IEnumerable<string> Get()
-        {
-            string[] words = { "bar", "foo", "foobar" };
-
-            return words;
-        }
-    }
 
     public class FileWordService : IWordService
     {
